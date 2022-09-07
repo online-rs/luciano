d56ca7102a84dfc225b2fe1d31195d4612a28d5a
online-rs <88519041+online-rs@users.noreply.github.com>
2022-09-07 20:53:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 //AQUI VAMOS COLCAR O H1 E OS LINKS QUE QUEREMOS
-botoes = ["Whatsapp", "Instagram", "Facebook", "Site"];
+botoes = ["Whatsapp", "Instagram", "Facebook"];
 var nomeprincipal = "LUCIANO DA SÃO LUIS";
-var instagramnome = "advbrunoo"
+var instagramnome = "luciano_sao_luis";
 var celular = "996993725";
-var facebooknome = "advbrunoo";
-var sitenome = "https://www.reiterlog.com"
-
-
+var facebooknome = "Lucianodasaoluis";
+var sitenome = "https://www.reiterlog.com";
 
 //inserindo o H1
 var newh1 = document.createElement("h1");
@@ -23,7 +21,7 @@ function criar(nome) {
 
   var rede = nome;
   const botaozinho = document.createElement("button");
-  botaozinho.setAttribute("class", "btn  btn-primary");
+  botaozinho.setAttribute("class", "btn btn-lg btn-primary");
   botaozinho.id = rede;
 
   botaozinho.innerHTML = rede;
@@ -34,13 +32,21 @@ botoes.forEach(criar);
 
 //gerando os links
 var a = document.getElementById("Whatsapp");
-a.onclick = function fwhatsapp(){window.open("https://wa.me/" + celular)};
+a.onclick = function fwhatsapp() {
+  window.open("https://api.whatsapp.com/send?phone=5551" + celular + "&text=Oi.%20Achei%20seu%20contato%20no%20instagram.%20Quer%20falar%20com%20voc%C3%AA%20sobre%3A");
+};
 
 var a = document.getElementById("Instagram");
-a.onclick = function fwhatsapp(){window.open("https://www.instagram.com/" + instagramnome)};
+a.onclick = function fwhatsapp() {
+  window.open("https://www.instagram.com/" + instagramnome);
+};
 
 var a = document.getElementById("Facebook");
-a.onclick = function fwhatsapp(){window.open("https://www.facebook.com/" + facebooknome)};
+a.onclick = function fwhatsapp() {
+  window.open("https://www.facebook.com/" + facebooknome);
+};
 
 var a = document.getElementById("Site");
-a.onclick = function fwhatsapp(){window.open(sitenome)};
+a.onclick = function fwhatsapp() {
+  window.open(sitenome);
+};
